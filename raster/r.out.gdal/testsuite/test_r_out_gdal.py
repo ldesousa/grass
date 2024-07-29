d76534f2a14f20bae33dad912b03bfc198ca217d
Luís de Sousa <luis.de.sousa@protonmail.ch>
2024-07-29 10:08:35 +0100

    Attempts to set the region in the setUpClass method

--- raster/r.out.gdal/testsuite/test_r_out_gdal.py
+++ raster/r.out.gdal/testsuite/test_r_out_gdal.py
@@ -38,6 +38,7 @@ sum=53745070
     def tearDownClass(cls):
         """!Remove the temporary region"""
         cls.del_temp_region()
+        cls.runModule("g.region", raster=cls.test_map)
 
     def tearDown(self):
         self.runModule(
@@ -49,7 +50,7 @@ sum=53745070
     def test_gpkg_format(self):
         """Tests output to GeoPackage format"""
 
-        self.runModule("g.region", raster=self.test_map)
+        #self.runModule("g.region", raster=self.test_map)
 
         self.assertModule(
             "r.out.gdal",
