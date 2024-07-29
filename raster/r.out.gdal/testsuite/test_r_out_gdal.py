d6f84e1b066922bd625f46cfe793d3f89197983c
Luís de Sousa <luis.de.sousa@protonmail.ch>
2024-07-29 15:42:54 +0100

    Moves all g.region commands to setUpClass method

--- raster/r.out.gdal/testsuite/test_r_out_gdal.py
+++ raster/r.out.gdal/testsuite/test_r_out_gdal.py
@@ -33,12 +33,12 @@ sum=53745070
     def setUpClass(cls):
         """Use temporary region settings"""
         cls.use_temp_region()
+        cls.runModule("g.region", raster=cls.test_map)
 
     @classmethod
     def tearDownClass(cls):
         """!Remove the temporary region"""
         cls.del_temp_region()
-        cls.runModule("g.region", raster=cls.test_map)
 
     def tearDown(self):
         self.runModule(
@@ -49,8 +49,6 @@ sum=53745070
 
     def test_gpkg_format(self):
         """Tests output to GeoPackage format"""
-
-        #self.runModule("g.region", raster=self.test_map)
 
         self.assertModule(
             "r.out.gdal",
@@ -77,8 +75,6 @@ sum=53745070
 
     def test_gtiff_format(self):
         """Tests output to GeoTiff format"""
-
-        self.runModule("g.region", raster=self.test_map)
 
         self.assertModule(
             "r.out.gdal",
